df2b543f6d53a571bf5c6cdf8717db924f1b92a0
protarie125 <protarie@gmail.com>
2023-11-25 01:30:50 +0900

    플로이드-워셜로 수정

--- BOJ_1389/src/main.cpp
+++ BOJ_1389/src/main.cpp
@@ -1,81 +1,70 @@
+#include <algorithm>
+#include <cmath>
 #include <iostream>
-#include <vector>
+#include <limits>
 #include <map>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-map<int, vector<int>> adjs{};
+using vi = vector<int>;
+using vvi = vector<vi>;
 
-int dist(int N, int from, int to) {
-    vector<bool> isVisited(N, false);
-    queue<pair<int, int>> q{};
-    q.push({ from, 0 });
+constexpr auto Inf = numeric_limits<int>::max() / 2 - 1;
 
-    while (!q.empty()) {
-        auto f = q.front();
-        if (to == f.first)
-            return f.second;
-
-        q.pop();
-        isVisited[f.first - 1] = true;
-
-        for (const auto& v : adjs[f.first])
-        {
-            if (!isVisited[v - 1]) {
-                q.push({ v, f.second + 1 });
-            }
-        }
-    }
-
-    return 987654321;
-}
-
-int solve(int N) {
-    int result{ 0 };
-    int minDist{ 987654321 };
-
-    for (int i = 1; i <= N; ++i) {
-        int sum{ 0 };
-        for (int j = 1; j <= N; ++j)
-        {
-            if (j == i)
-                continue;
-
-            sum += dist(N, i, j);
-        }
-
-        if (sum < minDist) {
-            minDist = sum;
-            result = i;
-        }
-    }
-
-    return result;
-}
+int N, M;
+vvi adj;
 
 int main() {
-    ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
 
-    int N, M;
-    cin >> N >> M;
+	cin >> N >> M;
 
-    for (int i = 1; i <= N; ++i)
-    {
-        adjs[i] = vector<int>{};
-    }
+	adj = vvi(N, vi(N, Inf));
+	while (0 < (M--)) {
+		int u, v;
+		cin >> u >> v;
 
-    for (int i = 0; i < M; ++i)
-    {
-        int a, b;
-        cin >> a >> b;
+		--u;
+		--v;
 
-        adjs[a].push_back(b);
-        adjs[b].push_back(a);
-    }
+		adj[u][v] = 1;
+		adj[v][u] = 1;
+	}
 
-    cout << solve(N);
+	for (auto k = 0; k < N; ++k) {
+		for (auto i = 0; i < N; ++i) {
+			for (auto j = 0; j < N; ++j) {
+				if (i == j) {
+					continue;
+				}
 
-    return 0;
+				adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+			}
+		}
+	}
+
+	auto sums = vi(N, 0);
+	for (auto i = 0; i < N; ++i) {
+		for (auto j = 0; j < N; ++j) {
+			if (i == j) {
+				continue;
+			}
+
+			sums[i] += adj[i][j];
+		}
+	}
+
+	auto ans = 0;
+	for (auto i = 0; i < N; ++i) {
+		if (sums[i] < sums[ans]) {
+			ans = i;
+		}
+	}
+
+	cout << ans + 1;
+
+	return 0;
 }
